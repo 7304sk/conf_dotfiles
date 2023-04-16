399f37a2ce301f7385f1c98be7ba80f0f28022d5
shoalwave <shoalwave.dev@gmail.com>
2023-04-16 20:18:26 +0900

    config.fish をちゃんと共通して動くように

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,13 +7,13 @@ alias rm='rm -i'
 #! global variables
 set -xg LANG ja_JP.UTF-8
 set -xg GREP_OPTIONS '--color=auto'
-set -xg PYTHONPATH $HOME/WORK/__lib__ $PYTHONPATH
+set -xg PYTHONPATH $HOME/__pylib__ $PYTHONPATH
+set -xg N_PREFIX $HOME/.n
 
 #! Configuration by OS
 if test (uname -s) = Darwin
     #! for Mac
-    eval /opt/anaconda3/bin/conda "shell.fish" hook $argv | source
-    set -xg PATH /opt/homebrew/opt/coreutils/libexec/gnubin /opt/homebrew/bin $PATH
+    set -xg PATH $N_PREFIX/bin /opt/homebrew/opt/coreutils/libexec/gnubin /opt/homebrew/bin $PATH
 else
     #! for other (Linux)
     eval ~/anaconda3/bin/conda "shell.fish" hook $argv | source
@@ -21,5 +21,3 @@ else
     alias update='echo -e "\n\e[33m##### Update list of packages. #####\e[m\n" && sudo apt update && echo -e "\n\e[33m##### List of packages is updated. #####\n##### NEXT: Upgrade all packages #####\e[m\n" && sudo apt upgrade && echo -e "\n\e[33m##### All installed packages are upgraded. #####\n##### NEXT: Remove packages that are no longer needed #####\e[m\n" && sudo apt autoremove && echo -e "\n\e[33m##### Packages that are no longer needed for the update are removed. #####\n##### NEXT: Clean up cached files #####\e[m\n" && sudo apt clean && echo -e "\n\e[33m##### all cached deb files are deleted. #####\e[m\n"'
 end
 
-#! initialize Python
-conda activate main
